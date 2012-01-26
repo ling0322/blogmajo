4db5488ba51f03f77611428a626c5478fde87ad4
ling0322 <ling032x@gmail.com>
2012-01-26 12:16:07 +0400

    some changes

--- main.py
+++ main.py
@@ -354,5 +354,5 @@ application = tornado.web.Application([
 ], **settings)
 
 if __name__ == "__main__":
-    application.listen(80)
+    application.listen(3323)
     tornado.ioloop.IOLoop.instance().start()
